f9e59cd75a039e3c6e5cd9f9a00d0e6f86a5ad03
fufukwang <fu@thenewslens.com>
2017-09-16 14:31:45 +0800

    add icon link

--- resources/views/TFO/m/_head.blade.php
+++ resources/views/TFO/m/_head.blade.php
@@ -5,3 +5,5 @@
     <script src="/T41/js/imageloaded.js"></script>
     <script src="/T41/js/m.main.js"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="/T41/T41.png" type="image/vnd.microsoft.icon" />
+	<link rel="icon" href="/T41/T41.png" type="image/vnd.microsoft.icon" />
