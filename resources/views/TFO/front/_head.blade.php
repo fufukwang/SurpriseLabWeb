8bab6a78c440a741373130cdb537e53191f4ec62
fufukwang <fu@thenewslens.com>
2017-09-18 22:31:36 +0800

    update og setting

--- resources/views/TFO/front/_head.blade.php
+++ resources/views/TFO/front/_head.blade.php
@@ -7,6 +7,8 @@
     <link rel="shortcut icon" href="/T41/T41.png" type="image/vnd.microsoft.icon" />
 	<link rel="icon" href="/T41/T41.png" type="image/vnd.microsoft.icon" />
 	<meta property="og:image" content="https://surpriselab.com.tw/T41/fb_share.jpg"/> 
+	<meta property="og:title" content="一人餐桌 table for ONE  | 一個人挺好，享受它。" >
+    <meta property="og:description" content="世界挺吵，一個人吃飯挺好。驚喜製造全新餐飲體驗，獻給在生活中，時不時需要一個人的 - 我們每一個人。" >
 	<!-- Global Site Tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-75329055-5"></script>
 <script>
