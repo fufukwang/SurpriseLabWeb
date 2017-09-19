98a8fd5b469cc34db3eb88ea030c71e02f312b78
fufukwang <fu@thenewslens.com>
2017-09-19 17:16:45 +0800

    fix viewport

--- resources/views/TFO/front/_head.blade.php
+++ resources/views/TFO/front/_head.blade.php
@@ -1,5 +1,5 @@
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width; initial-scale=1.0">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" href="/T41/style/css/print.css">
     <script src="/T41/js/jquery.js"></script>
     <script src="/T41/js/imageloaded.js"></script>
