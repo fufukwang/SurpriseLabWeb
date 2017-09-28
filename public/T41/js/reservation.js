c58339473072104269847cc6733501b69728c31a
fufukwang <fu@thenewslens.com>
2017-09-28 22:37:31 +0800

    fix error and ver

--- public/T41/js/reservation.js
+++ public/T41/js/reservation.js
@@ -40,7 +40,7 @@ $(function(){
     $('#goStep3').bind('click',function(){
         if($('#day').val() == ''){ $('#day').parent().parent().parent().addClass('error'); } else{ $('#day').parent().parent().parent().removeClass('error'); }
         if($('#datepart').val() == ''){ $('#datepart').parent().addClass('error'); } else { $('#datepart').parent().removeClass('error'); }
-        if($('#id').val() == ''){ $('#id').parent().addClass('error'); } else { $('#id').parent().removeClass('error'); $('#step2').hide(); $('#step3').show(); 
+        if($('#id').val() == ''){ $('#id').parent().addClass('error'); } else { $('#id').parent().removeClass('error'); $('#step2').hide(); $('#step3').show(); }
     });
 
     $('#datepart').bind('change',function(){
@@ -126,7 +126,7 @@ $(function(){
             $('#step3').hide();
             $('#step4').show();
             fbq('track', 'AddPaymentInfo');
-        }
+        
         }
     });
     /* step 4 */
