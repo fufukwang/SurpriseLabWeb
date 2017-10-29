c8fc07368cb6825b3ca706f08833a529aa7e11f8
fufukwang <fu@thenewslens.com>
2017-10-29 16:25:42 +0800

    change ver

--- resources/views/TFO/m/_head.blade.php
+++ resources/views/TFO/m/_head.blade.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="/T41/style/css/print.css?v=0.3">
     <script src="/T41/js/jquery.js"></script>
     <script src="/T41/js/imageloaded.js"></script>
-    <script src="/T41/js/m.main.js?v=0.1"></script>
+    <script src="/T41/js/m.main.js?v=0.2"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="/T41/T41.png" type="image/vnd.microsoft.icon" />
 	<link rel="icon" href="/T41/T41.png" type="image/vnd.microsoft.icon" />
