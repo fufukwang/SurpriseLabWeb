c5d6865f56d5edc5f2b7c61a4e3ddee39d063ecf
fufukwang <fu@thenewslens.com>
2017-12-16 23:09:11 +0800

    change tfogift

--- app/model/TFOGift.php
+++ app/model/TFOGift.php
@@ -13,7 +13,7 @@ class TFOGift extends Model
      *
      * @var array
      */
-    protected $fillable = ['code','satatus','paystatus','tfoorder_id','bname','btel','bemail','rname','rtel','remail','address','send','manage','admin_id','sendtype','backresult','result','InvitionText','sn'];
+    protected $fillable = ['code','satatus','paystatus','tfoorder_id','bname','btel','bemail','rname','rtel','remail','address','send','manage','admin_id','sendtype','backresult','result','InvitationText','sn'];
     protected $guarded = [];
     protected $primaryKey = 'id';
     protected $dates = ['created_at', 'updated_at'];
