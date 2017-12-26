8b09392e2c625bc67787750e1e6d9abe8b9ca085
fufukwang <fu@thenewslens.com>
2017-12-26 23:26:29 +0800

    修改 fb pixel

--- resources/views/TFO/front/giftcard5.blade.php
+++ resources/views/TFO/front/giftcard5.blade.php
@@ -4,25 +4,7 @@
         table for ONE 一人餐桌
     </title>
     @include('TFO.front._head')
-<!-- Facebook Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window,document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '169783437091349'); 
-fbq('track', 'Purchase', {value:'1200', currency:'TWD'});
-</script>
-<noscript>
-<img height="1" width="1" 
-src="https://www.facebook.com/tr?id=169783437091349&ev=Purchase
-&noscript=1"/>
-</noscript>
-<!-- End Facebook Pixel Code -->
+
 </head>
 <body>
     <div class="wrapper" id="red-theme" key="1">    
