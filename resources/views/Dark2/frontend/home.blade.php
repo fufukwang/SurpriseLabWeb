03c724dbeeb93cb9b159b9946fbc22d2970b0fea
fufukwang <fu@thenewslens.com>
2018-05-03 23:21:30 +0800

    fix text

--- resources/views/Dark2/frontend/home.blade.php
+++ resources/views/Dark2/frontend/home.blade.php
@@ -165,7 +165,7 @@
                                     </p>
                                 </li>
                                 <li>
-                                    <div class="date">05/31</div>
+                                    <div class="date">06/04</div>
                                     <div class="dot"></div>
                                     <p>
                                         預售截止
@@ -213,7 +213,7 @@
                         </div>
                         <div class="hashtag-box season-two-about">
                             <h1>
-                                #Hashtag
+                                #無光晚餐
                             </h1>
                             <div class="hashtag-container"><div><iframe src="https://live.memopresso.com/embed?token=albert&title=new&titlePosition=0&displayType=waterfall&initCount=20&showLoadMore=true&showLogo=false&showTitle=false&contentColor=999999&tagColor=89229b&displaySequence=RANDOM&" id="memopresso_embed" width="100%" scrolling="No" frameborder="0"></iframe><script type="text/javascript">window.onmessage = function(event) {document.getElementById("memopresso_embed").height = event.data};setInterval(function(){document.getElementById("memopresso_embed").contentWindow.postMessage("","*");}, 500);</script></div></div>
                             <a href="pre-order.html">
