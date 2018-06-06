2773798c43b5c3121aa3bb6198d7d2f7d593174f
fufukwang <fu@thenewslens.com>
2018-06-06 20:32:03 +0800

    add files

---
+++ public/dark2/js/reservation.js
@@ -0,0 +1,334 @@
+var coupon = [];
+var discount = '';
+$(function(){
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    var showdate = [];
+    $('.date_tr').hide();
+    // 第一步
+    $('#first-btn').bind('click',function(){
+        console.log('');
+        if($('#agree').prop('checked')){
+            $('#agree-error').hide();
+            ChangeSectionNav('second','first',1);
+        } else {
+            $('#agree-error').show();
+        }
+    });
+    // 第二步
+    $('#second-btn-back').bind('click',function(){
+        ChangeSectionNav('first','second',0);
+    });
+    $('#second-btn').bind('click',function(){
+        // 驗證選項
+        var GoNext = 0;
+        if($('#Pople').val()==0){
+            $('#PopleAlert').show(); GoNext++;
+        } else {
+            $('#PopleAlert').hide();
+        }
+        if($('#pro_id').val()==0 || $('#pro_id option:selected').data('pople')<$('#Pople').val() || $('#pro_id').val()==null){
+            $('#pro_idAlert').show(); GoNext++;
+        } else {
+            $('#pro_idAlert').hide();
+        }
+        if(GoNext==0){
+            // creat table 
+            var html = '<tr><td></td><th>葷食</th><th>素食</th></tr>';
+            for(i=0;i<$('#Pople').val();i++){
+                var Num = i+1;
+                html += '<tr><td>顧客'+Num+'</td><th><input id="Pople_H'+Num+'" name="Pople['+Num+']" checked value="Hunsi" type="radio"><label for="Pople_H'+Num+'"><span></span></label></th><th><input id="Pople_V'+Num+'" name="Pople['+Num+']" type="radio" value="Vegetarian"><label for="Pople_V'+Num+'"><span></span></label></th></tr>';
+            }
+            $('#MealTable').html(html);
+            ChangeSectionNav('third','second',2);
+        }
+    });
+
+    // 第三步
+    $('#third-btn-back').bind('click',function(){
+        ChangeSectionNav('second','third',1);
+    });
+    $('#third-btn').bind('click',function(){
+        // 驗證資料
+        var GoNext = 0;
+        if($('#Name').val()==''){ $('#NameAlert').show(); GoNext++; } else { $('#NameAlert').hide(); }
+        if(validateTel($('#Tel').val())){ $('#TelAlert').show(); GoNext++; } else { $('#TelAlert').hide(); }
+        if(!validateEmail($('#EMail').val())){ $('#EMailAlert').show(); GoNext++; } else { $('#EMailAlert').hide(); }
+        if(!$('#privatecheck').prop('checked')){ $('#PrivateAlert').show(); GoNext++; } else { $('#PrivateAlert').hide(); }
+        if(GoNext==0){
+            $('#PopCheck').text($('#Pople').val());
+            $('#ADayCheck').text($('#ADay').val());
+            $('#STimeCheck').text($('#pro_id option:selected').text());
+            $('#NameCheck').text($('#Name').val());
+            $('#TelCheck').text($('#Tel').val());
+            $('#EMailCheck').text($('#EMail').val());
+            var text = '';
+            for(i=0;i<$('input[value=Hunsi]').length;i++){
+                text += text=='' ? '' : '/';
+                var tmpval = i + 1;
+                if($('#Pople_H'+tmpval).prop('checked')){
+                    text += '葷食';
+                } else {
+                    text += '素食';
+                }
+            }
+            $('#MealCheck').text(text);
+            $('#NotesCheck').text($('#Notes').val());
+            var money = $('#pro_id option:selected').data('money') * $('#Pople').val();
+            var cash  = $('#pro_id option:selected').data('cash') * $('#Pople').val();
+            $('#MoneyView').text(cash);
+            $('#CardView').text(money);
+            $('#MoneyView').data('money',cash).data('cut1',0).data('cut2',0);
+            $('#CardView').data('money',money).data('cut1',0).data('cut2',0);
+            ChangeSectionNav('fourth','third',3);
+        }
+    });
+
+    // 第四步
+    $('#fourth-btn-back').bind('click',function(){
+        ChangeSectionNav('third','fourth',2);
+    });
+    $('#fourth-btn-onsite').bind('click',function(){
+        // 送出資料
+        SendOrderData('onsite','');
+    });
+    $('#fourth-btn-online').bind('click',function(){
+        // 轉到金流
+        //SendOrderData('online');
+        $('.lightbox2pay').fadeToggle(700);
+    });
+    $('#fourth-btn-zero').bind('click',function(){
+        var money = $('#MoneyView').data('money') - $('#MoneyView').data('cut1') - $('#MoneyView').data('cut2');
+        if( Math.max(money,0) === 0 ){
+            SendOrderData('onsite','');
+        } else {
+            alert('目前僅供使用優惠券使用!');
+        }
+    });
+
+
+    //$('#first-section').val();
+    $('#SurePay').bind('click',function(){
+        TPDirect.card.getPrime(function(result) {
+            if (result.status !== 0) {
+                alert('卡片資料有誤!無法取得認證!');
+                console.log('getPrime error');
+                return false;
+            }
+            var prime = result.card.prime
+            console.log('getPrime success');
+            $('.lightbox2pay').fadeToggle(700);
+            SendOrderData('online',prime);
+        })
+    });
+    $('#CanalPay').bind('click',function(){
+        $('.lightbox2pay').fadeToggle(700);
+    });
+    $('#CouponBtn').bind('click',function(){
+        $.blockUI();
+        var code = $('#code').val().toLowerCase();
+        if($('#CardView').text()==0){
+            alert('已不需要使用優惠券!');
+            return false;
+        }
+        if(code!=''){
+            var cut = 0;
+            switch(code){
+                case 'dininginthedarks1': cut = 200; discount = 'dininginthedarks1'; break;
+                case 'tableforone41': cut = 200; discount = 'tableforone41'; break;
+                case 'gqinthedark': cut = 100; discount = 'gqinthedark'; break;
+                case 'juksyinthedark': cut = 100; discount = 'juksyinthedark'; break;
+                case 'vogueinthedark': cut = 100; discount = 'vogueinthedark'; break;
+                case 'sdinthedark': cut = 100; discount = 'sdinthedark'; break;
+            }
+            $('#MoneyView,#CardView').data('cut2',cut);
+            changeMoneyView();
+            if(discount != '' && cut > 0){
+                $('#discountList').text('折扣碼:'+discount+'折扣 ' +cut+ ' 元');
+            }
+            if(cut==0){
+                code = code.toUpperCase();
+                $.get('/dininginthedark2/GetAjaxData',{'act':'CheckCoupon','code':code,'coupon':coupon},function(data){
+                    if(data.success == 'Y'){
+                        coupon.push(code);
+                        var money = $('#pro_id option:selected').data('money') * (coupon.length * 2);
+                        var cash  = $('#pro_id option:selected').data('cash') * (coupon.length * 2);
+                        $('#MoneyView').data('cut1',cash);
+                        $('#CardView').data('cut1',money);
+                        changeMoneyView();
+                        var num  = $('#couponList').find('br').length + 1;
+                        var wine = '';
+                        if(data.wine>0) wine = '(含調飲)';
+                        $('#couponList').append('優惠序號'+ num +':'+code+'雙人套票'+wine+'<br>');
+            
+                    }
+                },'json');
+            }
+            $('#code').val('');
+        } else {
+            alert('請輸入優惠碼!');
+        }
+        $.unblockUI();
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+    $('#dayparts').bind('change',function(){
+        var val = $(this).val();
+        var day = $(this).data('day');
+        $.get('/dininginthedark2/GetAjaxData',{'act':'getBydartpart','dayparts':val,'day':day,'pople':$('#Pople').val()},function(data){
+            var nowdata = [];
+            spday = [],oneday = [];
+            $('#pro_id option').remove();
+            $('#pro_id').append('<option value="">請選擇時段</option>');    
+            for(i=0;i<data.length;i++){
+                var disable = '';
+                var fulltxt = '';
+                /*
+                if(parseInt(data[i].Pople)<parseInt($("#Pople").val())){
+                    disable = 'disabled';
+                    fulltxt = '(額滿)';
+                }*/
+                $('#pro_id').append('<option value="'+data[i].id+'" data-cash="'+data[i].cash+'" data-money="'+data[i].money+'" '+disable+'>'+data[i].rangstart.substr(0,5)+'-'+data[i].rangend.substr(0,5)+' '+fulltxt+'</option>');
+
+            }
+
+        },'json');
+    });
+
+    $('#pro_id').bind('change',function(){
+        if($('#pro_id option:selected').data('pople')>$('#Pople').val()){
+            $('#pro_idAlert').hide();
+        }
+    });
+
+
+    TPDirect.setupSDK('12126', 'app_XjlfXqQFdxLlY1JJGIJ6beAdppcgzciaeo356asbZ5ASVzD2Pryt0dErylTj', 'sandbox');
+    TPDirect.card.setup({
+        fields: {
+            number: {
+                // css selector
+                element: '#card-number',
+                placeholder: '**** **** **** ****'
+            },
+            expirationDate: {
+                // DOM object
+                element: document.getElementById('card-expiration-date'),
+                placeholder: 'MM / YY'
+            },
+            ccv: {
+                element: '#card-ccv',
+                placeholder: 'ccv'
+            }
+        },
+        styles: {
+            // Style all elements
+            'input': {
+                'color': 'gray'
+            },
+            // Styling ccv field
+            'input.cvc': {
+                // 'font-size': '16px'
+            },
+            // Styling expiration-date field
+            'input.expiration-date': {
+                // 'font-size': '16px'
+            },
+            // Styling card-number field
+            'input.card-number': {
+                // 'font-size': '16px'
+            },
+            // style focus state
+            ':focus': {
+                // 'color': 'black'
+            },
+            // style valid state
+            '.valid': {
+                'color': 'green'
+            },
+            // style invalid state
+            '.invalid': {
+                'color': 'red'
+            },
+            // Media queries
+            // Note that these apply to the iframe, not the root window.
+            '@media screen and (max-width: 400px)': {
+                'input': {
+                    'color': 'orange'
+                }
+            }
+        }
+    });
+
+});
+function SendOrderData(Pay,prime){
+    $.blockUI();
+    var Meal = [];
+    for(i=0;i<$('input[value=Hunsi]').length;i++){
+        var tmpval = i + 1;
+        Meal[i] = $('#Pople_H'+tmpval).prop('checked') ? '葷' : '素';
+    }
+    var obj = {
+        'name'  : $('#Name').val(),
+        'tel'   : $('#Tel').val(),
+        'email' : $('#EMail').val(),
+        'notes' : $('#Notes').val(),
+        'pro_id': $('#pro_id').val(),
+        'Pople' : $('#Pople').val(),
+        'prime' : prime,
+        'Meal'  : Meal,
+        'Pay'   : Pay,
+        'coupon': coupon,
+        'Lng'   : '',
+        'discount':discount,
+    };
+    $.post('/dininginthedark2/ReOrderData',obj,function(data){
+        if(data.success==true){
+            ChangeSectionNav('done','fourth',4); 
+        } else {
+            alert('本場次人數已滿!');
+            //$.unblockUI();
+        }
+        $.unblockUI();
+    },'json').fail(function() {
+        alert('服務繁忙中請稍後在試!');
+        $.unblockUI();
+    }).always(function() {
+        
+    });
+}
+
+
+
+function changeMoneyView(){
+    var money = $('#MoneyView').data('money') - $('#MoneyView').data('cut1') - $('#MoneyView').data('cut2');
+    $('#MoneyView').text(Math.max(money,0));
+    var money = $('#CardView').data('money') - $('#CardView').data('cut1') - $('#CardView').data('cut2');
+    $('#CardView').text(Math.max(money,0));
+}
+
+function ChangeSectionNav(show,hide,nav){
+    $('.reservation-nav ul li').removeClass('active');
+    $('.reservation-nav ul li:eq('+nav+')').addClass('active');
+    $('#'+hide+'-section').hide();
+    $('#'+show+'-section').show();
+}
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+function validateTel(tel){
+    if(tel==''){ return true; } else if(tel.length<9){ return true; } else if(tel.length>12) { return true; } else { return false; }
+}
---
+++ resources/views/Dark2/backend/coupons.blade.php
@@ -0,0 +1,180 @@
+@include('backstage.header')
+<!-- =======================
+             ===== START PAGE ======
+             ======================= -->
+
+        <div class="wrapper">
+            <div class="container">
+
+                <!-- Page-Title -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h4 class="page-title">兌換碼 </h4>
+                    </div>
+                </div>
+                <!-- Page-Title -->
+                
+                
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="card-box">
+
+
+                            <div class="table-rep-plugin">
+                                <div class="table-wrapper">
+                                    <div class="btn-toolbar">
+                                        <div class="btn-group focus-btn-group"><form action="/darks/coupons" id="SearchForm">
+
+                                            <!--div class="form-group col-sm-2">
+                                                <div class="col-sm-12">
+                                                    <div class="input-group">
+                                                        <input type="text" class="form-control" placeholder="yyyy-mm-dd" name="day" id="datepicker-autoclose" value="{{ $request->day or ''}}">
+                                                        <span class="input-group-addon bg-primary b-0 text-white"><i class="ion-calendar"></i></span>
+                                                    </div>
+                                                </div>
+                                            </div-->
+                                            <div class="form-group col-sm-8">
+                                                <div class="col-sm-12">
+                                                    <div class="input-group">
+                                                        <input type="text" class="form-control" placeholder="搜尋" name="search" id="datepicker-autoclose" value="{{ $request->search or ''}}">
+                                                    </div><!-- input-group -->
+                                                </div>
+                                            </div>
+
+                                            <button type="submit" class="btn btn-info"><span class="glyphicon glyphicon-search"></span> 搜尋</button>&nbsp;
+                                            <!--button type="button" class="btn btn-info" onclick="submitSearchForm();" style="margin-left: 10px;"><span class="glyphicon glyphicon-print"></span> 列印</button>
+                                            <button type="button" class="btn btn-info" onclick="submitPrintCover();" style="margin-left: 10px;"><span class="glyphicon glyphicon-print"></span> 封面列印</button-->
+
+
+                                        </form></div>
+                                    </div><div class="table-responsive" data-pattern="priority-columns">
+                                    <div class="sticky-table-header fixed-solution" style="width: auto;"><table id="tech-companies-1-clone" class="table table-striped table-hover">
+                                        <thead>
+                                            <tr>
+                                                <th>Code</th>
+                                                <th>含飲料</th>
+                                                <th>兌換日期</th>
+                                                <!--th>功能</th-->
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+@forelse ($coupons as $row)
+                                            <tr id="tr_{{ $row->id }}">
+                                                <td>{{ $row->code }}</td>
+                                                <td>@if( $row->wine ) 包含 @else 不包含 @endif</td>
+                                                <td>@if($row->order_id > 0) 
+                                                    {{ App\model\d2order::where('sn',$row->order_id)->first()->created_at }}
+                                                @else 尚未兌換
+                                                @endif</td>
+                                                <!--td class="actions">
+                                                    <a class="btn btn-primary btn-xs" href="/TableForOne/gift/{{ $row->id }}/edit"><i class="fa fa-pencil"></i></a>
+                                                    <a class="btn btn-danger btn-xs" href="javascript:;" data-id={{ $row->id }}><i class="fa fa-remove"></i></a>
+                                                </td-->
+                                            </tr>
+@empty
+<tr><td colspan="13" align="center">尚無資料</td></tr>
+@endforelse
+                                        </tbody>
+                                    </table>
+
+
+                                    <div class="text-align-center">{{ $coupons->links() }}</div>
+                                </div></div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+                <!-- Footer -->
+                <footer class="footer text-right">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                2016 © Minton.
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+                <!-- End Footer -->
+
+            </div> <!-- end container -->
+        </div>
+        <!-- End wrapper -->
+
+@include('backstage.jquery')
+<link href="/backstage/plugins/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css" rel="stylesheet">
+<script src="/backstage/plugins/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="/backstage/plugins/bootstrap-datepicker/dist/locales/bootstrap-datepicker.zh-TW.min.js"></script>
+<link href="/backstage/plugins/sweetalert/dist/sweetalert.css" rel="stylesheet" type="text/css">
+
+<!-- Examples -->
+	    <script src="/backstage/plugins/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
+	    <script src="/backstage/plugins/jquery-datatables-editable/jquery.dataTables.js"></script>
+	    <script src="/backstage/plugins/datatables/dataTables.bootstrap.js"></script>
+	    <script src="/backstage/plugins/tiny-editable/mindmup-editabletable.js"></script>
+	    <script src="/backstage/plugins/tiny-editable/numeric-input-example.js"></script>
+
+	    <script src="/backstage/pages/datatables.editable.init.js"></script>
+        <script src="/backstage/plugins/sweetalert/dist/sweetalert.min.js"></script>
+
+
+        <script src="/backstage/js/jquery.core.js"></script>
+        <script src="/backstage/js/jquery.app.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.min.js"></script>
+        <!-- Notification js -->
+        <script src="/backstage/plugins/notifyjs/dist/notify.min.js"></script>
+        <script src="/backstage/plugins/notifications/notify-metro.js"></script>
+
+        <script>
+        //$('#datatable').dataTable();
+			//$('#mainTable').editableTableWidget().numericInputExample().find('td:first').focus();
+$(function(){
+    $('.sendbox').bind('click',function(){
+        var send = $(this).prop('checked');
+        var id   = $(this).val();
+        $.post('/dark2/conpou/'+id+'/sendUpdate',{
+            send : (send ? 1 : 0)
+        },function(data){
+            $.Notification.notify('success','bottom left','已更新', '發送狀態已更新')
+        },'json');
+        console.log(send);
+    });
+    $('.btn-danger').bind('click',function(){
+        var id = $(this).data('id');
+        if(confirm("確定要刪除禮物卡?如有訂單將一併刪除")) {
+             $.ajax({
+                url: '/dark2/conpou/'+id+'/delete',
+                method: 'delete',
+                dataType:'json'
+            }).done(function(data){
+                swal(data.message);
+                $('#tr_'+id).remove();
+            });
+        }
+    });
+
+
+});
+
+@if(Session::has('message')) alert('{{ Session::get('message') }}'); @endif
+		</script>
+
+
+    </body>
+</html>
+
