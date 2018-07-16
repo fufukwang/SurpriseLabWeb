652859e7c51d042561640e5d0e795a585ca9426e
fufukwang <fu@thenewslens.com>
2018-07-16 16:07:28 +0800

    fix text

--- resources/views/Dark2/frontend/reservation_pay.blade.php
+++ resources/views/Dark2/frontend/reservation_pay.blade.php
@@ -96,9 +96,9 @@
            
             <div class="main-container" id="first-section" style="padding-top: 0px;">
                 <div style="color:#a67c52;">
-<div style="font-weight:bold;font-size:24px;">第階段7/4-9/16座位已全數額滿</div>
-將不定時釋出座位。可留意訂位系統<br><br>
-若無法訂位但也想購票者，可至<a href="pre-sale.html" style="color:#a67c52;">第二階段預售</a>購票 (連結至 Pre Dale頁面)<br>
+<div style="font-weight:bold;font-size:24px;">第一階段7/4-9/16座位已全數額滿</div>
+將不定時釋出座位，可留意訂位系統<br><br>
+若無法訂位但也想購票者，可至<a href="pre-sale.html" style="color:#a67c52;">第二階段預售</a>購票<br>
 第二階段將開放9/22-11/4之座位
                 </div>
                 <p>
