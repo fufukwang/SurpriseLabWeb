f0fcfecf97b124ca67b34382da9adb6ac6d28812
fufukwang <fu@thenewslens.com>
2018-11-02 15:39:25 +0800

    time change

--- config/setting.php
+++ config/setting.php
@@ -2,9 +2,9 @@
 return [
     'per_page' => '20',
     'enddate'  => '2016-12-31',
-    'tfoend'   => '2018-12-31',
+    'tfoend'   => '2019-02-08',   // 後台時間
     'dark2' => [
     	'path'    => 'dininginthedark2',
-    	'enddate' => '2019-02-08'
+    	'enddate' => '2018-12-02'  // 前台開放時間
     ]
 ];
