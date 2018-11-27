e5057f51df9b51df292652ba3deba416b57b7725
fufukwang <fu@thenewslens.com>
2018-11-27 22:13:30 +0800

    修改日期範圍

--- config/setting.php
+++ config/setting.php
@@ -2,9 +2,9 @@
 return [
     'per_page' => '20',
     'enddate'  => '2016-12-31',
-    'tfoend'   => '2019-02-08',   // 後台時間
+    'tfoend'   => '2019-03-31',   // 後台時間
     'dark2' => [
     	'path'    => 'dininginthedark2',
-    	'enddate' => '2019-02-08'  // 前台開放時間
+    	'enddate' => '2019-03-31'  // 前台開放時間
     ]
 ];
