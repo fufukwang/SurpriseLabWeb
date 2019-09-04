9ae82bb8f8d1774b26ee4b0c06935039323c5263
fufukwang <fu@thenewslens.com>
2019-09-04 23:13:33 +0800

    新增路由與頁面

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -491,7 +491,8 @@ Route::group(['middleware' => ['web']], function () {
         Route::get('index.html',function(){ return view('clubtomorrow.frontend.home'); });
         Route::get('rules.html',function(){ return view('clubtomorrow.frontend.rules'); });
         Route::get('radio.html',function(){ return view('clubtomorrow.frontend.radio'); });
-
+        // 劃位
+        Route::get('booking.html',function(){ return view('clubtomorrow.frontend.booking'); });
 
         // 驗證SMS & SMS寄送
         Route::post('getting_intro_sms','clubT\FrontController@getting_intro_sms');
