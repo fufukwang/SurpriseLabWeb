2a650cb8686e672f2a0961b1e52c161c6cd7a9db
fufukwang <fu@thenewslens.com>
2019-09-20 21:51:30 +0800

    排程內容修改

--- app/Console/Commands/ClubSentMail.php
+++ app/Console/Commands/ClubSentMail.php
@@ -44,10 +44,25 @@ class ClubSentMail extends Command
     {
         try {
             // 取得前一天的內容
+            config(['mail.username' => env('MAIL_CLUB_USER')]);
+            config(['mail.password' => env('MAIL_CLUB_PASS')]);
             $orders = order::whereRaw("TO_DAYS(NOW())-TO_DAYS(created_at)=1")->where('pay_status','已付款')->get();
             foreach ($orders as $order) {
                 // sent email
+                $mailer = [
+                    'email' => $order->email,
+                    'name'  => $order->name,
+                ];
                 
+                Mail::send('clubtomorrow.email.t12',$mailer,function($m) use ($mailer){
+                    $m->from('clubtomorrow@surpriselab.com.tw', '明日俱樂部');
+                    $m->sender('clubtomorrow@surpriselab.com.tw', '明日俱樂部');
+                    $m->replyTo('clubtomorrow@surpriselab.com.tw', '明日俱樂部');
+
+                    $m->to($mailer['email'], $mailer['name']);
+                    $m->subject('明日俱樂部Emily來信請建立你的玩家ID!');
+                });
+                /*
                 // sent sms
                 $phone = $order->tel;
                 if($order->dial_code == "+886"){
@@ -57,6 +72,7 @@ class ClubSentMail extends Command
                 }
                 $message = "有你的信件喔";
                 $sent_obj = SLS::sent_single_sms($phone_number,$message);
+                */
             }
 
         } catch (Exception $exception) {
