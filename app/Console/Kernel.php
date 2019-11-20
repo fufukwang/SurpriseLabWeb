cfc7cbe2b38544be1e3b5b06075990bf53c72868
fufukwang <fu@thenewslens.com>
2019-11-20 21:22:48 +0800

    取消發送 12 點的信件

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
         //$schedule->command('sms:sent')->dailyAt('8:00');
         //$schedule->command('inspire')->hourly();
         // 中午12點寄送信件及簡訊
-        $schedule->command('club:sentmail')->dailyAt('12:00');
+        // $schedule->command('club:sentmail')->dailyAt('12:00');  // 191121 取消寄送
 
     }
 }
