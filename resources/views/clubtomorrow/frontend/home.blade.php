1f1bc1e9ff080fc1b84068a730cd7e47e98e6aea
RobinPaul <paul88543@gmail.com>
2019-12-10 00:08:32 +0800

    update home.js version

--- resources/views/clubtomorrow/frontend/home.blade.php
+++ resources/views/clubtomorrow/frontend/home.blade.php
@@ -803,7 +803,7 @@
 <!-- Custom Js -->
 <script src="/clubT/js/main.js"></script>
 <script src="/clubT/js/smooth-scroll.js"></script>
-<script src="/clubT/js/home.js?v=0.1"></script>
+<script src="/clubT/js/home.js?v=0.2"></script>
 <script src="/clubT/js/rotate-device-hint.js"></script>
 </body>
 </html>
