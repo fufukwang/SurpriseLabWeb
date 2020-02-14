145c135f9a357dbd2e39b7b293e2f336a695246a
Fay Sie <sel12342001@gmail.com>
2020-02-14 19:58:02 +0800

    update modal button styling

--- resources/views/clubtomorrow/frontend/home.blade.php
+++ resources/views/clubtomorrow/frontend/home.blade.php
@@ -34,7 +34,7 @@
     </script>
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="/clubT/css/style.css?v=0.2"/>
+    <link rel="stylesheet" href="/clubT/css/style.css?v=0.3"/>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     @include('clubtomorrow.frontend._gtm')
