cb8a5b560bbaab3340d361c58d709c032e969e04
Fay Sie <sel12342001@gmail.com>
2020-02-17 16:35:18 +0800

    update metadata

--- resources/views/clubtomorrow/frontend/_metadata.blade.php
+++ resources/views/clubtomorrow/frontend/_metadata.blade.php
@@ -1,10 +1,10 @@
     <meta charset="UTF-8">
     <meta name="keywords" content="驚喜製造、明日俱樂部、明日、劇場、表演">
-    <meta name="description" content="在你所熟悉的世界之外，還有另一個世界同時存在⋯ 你，準備好遊戲其中了嗎？">
+    <meta name="description" content="一個世界、五大陣營、六十位玩家，在分裂的世界中，你，能成為最後贏家嗎？">
     <meta property="og:type" content="website"/>
-    <meta property="og:title" content="明日俱樂部 Club Tomorrow"/>
-    <meta property="og:site_name" content="明日俱樂部 Club Tomorrow"/>
-    <meta property="og:description" content="在你所熟悉的世界之外，還有另一個世界同時存在⋯ 你，準備好遊戲其中了嗎？"/>
+    <meta property="og:title" content="歡迎來到明日俱樂部 Welcome to Club Tomorrow"/>
+    <meta property="og:site_name" content="歡迎來到明日俱樂部 Welcome to Club Tomorrow"/>
+    <meta property="og:description" content="一個世界、五大陣營、六十位玩家，在分裂的世界中，你，能成為最後贏家嗎？"/>
     <meta property="og:image" content="https://www.surpriselab.com.tw/clubT/img/og/og-2_PhoneNumberCollection.jpg"/>
     <meta property="og:image:type" content="image/png"/>
     <meta property="og:image:width" content="600"/>
