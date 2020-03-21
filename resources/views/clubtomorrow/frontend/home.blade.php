a0e2a4331c55e0c50f7c7de4cd993664767bea36
Fay Sie <sel12342001@gmail.com>
2020-03-21 17:44:24 +0800

    update ticket modal mobile styling

--- resources/views/clubtomorrow/frontend/home.blade.php
+++ resources/views/clubtomorrow/frontend/home.blade.php
@@ -34,7 +34,7 @@
     </script>
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="/clubT/css/style.css?v=0.8"/>
+    <link rel="stylesheet" href="/clubT/css/style.css?v=0.9"/>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     @include('clubtomorrow.frontend._gtm')
