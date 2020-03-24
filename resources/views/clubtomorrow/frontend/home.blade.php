9055745a0d01426577eddcf2bbfb2e2726700169
Fay Sie <sel12342001@gmail.com>
2020-03-24 13:24:17 +0800

    override button focus styling

--- resources/views/clubtomorrow/frontend/home.blade.php
+++ resources/views/clubtomorrow/frontend/home.blade.php
@@ -34,7 +34,7 @@
     </script>
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="/clubT/css/style.css?v=2.1"/>
+    <link rel="stylesheet" href="/clubT/css/style.css?v=2.2"/>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     @include('clubtomorrow.frontend._gtm')
