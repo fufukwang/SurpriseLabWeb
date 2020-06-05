e8cd43d766c5dba02c3a36bb8f73dd1002847b4b
Fay Sie <sel12342001@gmail.com>
2020-06-05 13:43:22 +0800

    update ending modal style in extra small mobile version

--- resources/views/clubtomorrow/frontend/home.blade.php
+++ resources/views/clubtomorrow/frontend/home.blade.php
@@ -37,7 +37,7 @@
     </script>
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="/clubT/css/style.css?v=3.8"/>
+    <link rel="stylesheet" href="/clubT/css/style.css?v=3.9"/>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     @include('clubtomorrow.frontend._gtm')
