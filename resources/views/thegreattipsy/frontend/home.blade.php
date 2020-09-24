d001d137c906914ef2e51db4322edfb8686acca8
Fay Sie <sel12342001@gmail.com>
2020-09-24 15:34:35 +0800

    revert ticket type section

--- resources/views/thegreattipsy/frontend/home.blade.php
+++ resources/views/thegreattipsy/frontend/home.blade.php
@@ -430,38 +430,40 @@
             <div class="ticket-type">
                 
                 <div class="type-item type-matinee">
-                    <a href="https://surpriselab.backme.tw/checkout/1056/6558?locale=zh-TW
+                    <a href="https://surpriselab.backme.tw/checkout/1397/10432?locale=zh-TW
                 " class="ticket-frame" target="_blank">
                         <img class="img-fluid d-none d-sm-block" src="img/tickets/ticket_face_1.png" alt="單人自在票">
                         <img class="img-fluid d-block d-sm-none" src="img/tickets/ticket_face_1_mobile.png" alt="單人自在票">
+                        <p class="ticket-state d-none d-sm-block">
+                            <!--已售完
+                            四人以上未滿八人者<br/>
+                            可依數量加購原價暢行無阻票-->
+                        </p>
+                        <p class="ticket-state d-block d-sm-none">
+                            <!--已售完四人以上未滿八人者 可依數量加購原價暢行無阻票-->
+                        </p>
+
+                    </a>
+                </div>
+                <div class="type-item type-great-tipsy">
+                    <a href="https://surpriselab.backme.tw/checkout/1397/10431?locale=zh-TW
+                " class="ticket-frame" target="_blank">
+                        <img class="img-fluid d-none d-sm-block" src="img/tickets/ticket_face_0.png" alt="驚喜老客人限定">
+                        <img class="img-fluid d-block d-sm-none" src="img/tickets/ticket_face_0_mobile.png" alt="驚喜老客人限定">
                         <p class="ticket-state timeLimit">
                             限量 <span class="highlight total-ticket">500</span> 張 | 剩餘 <span
                                 class="highlight rest-ticket">XXX</span> 張
                         </p>
                     </a>
                 </div>
-                <div class="type-item type-great-tipsy sold">
-                    <a href="https://surpriselab.backme.tw/checkout/1056/6559?locale=zh-TW
-                " class="ticket-frame" target="_blank">
-                        <img class="img-fluid d-none d-sm-block" src="img/tickets/ticket_face_0_soldout.png" alt="驚喜老客人限定">
-                        <img class="img-fluid d-block d-sm-none" src="img/tickets/ticket_face_0_soldout_mobile.png" alt="驚喜老客人限定">
-                        <p class="ticket-state timeLimit">
-                            已售完
-                        </p>
-                    </a>
-                </div>
                 <div class="type-item type-group">
-                    <a href="https://surpriselab.backme.tw/checkout/1056/6560?locale=zh-TW
+                    <a href="https://surpriselab.backme.tw/checkout/1397/10433?locale=zh-TW
                     " class="ticket-frame" target="_blank">
                         <img class="img-fluid d-none d-sm-block" src="img/tickets/ticket_face_3.png" alt="六人共享票">
                         <img class="img-fluid d-block d-sm-none" src="img/tickets/ticket_face_3_mobile.png" alt="六人共享票">
-                        <p class="ticket-state d-none d-sm-block">
-                            已售完<!--
-                            四人以上未滿八人者<br/>
-                            可依數量加購原價暢行無阻票-->
-                        </p>
-                        <p class="ticket-state d-block d-sm-none">
-                            已售完<!--四人以上未滿八人者 可依數量加購原價暢行無阻票-->
+                        <p class="ticket-state sixShare">
+                            限量 <span class="highlight total-ticket">500</span> 張 | 剩餘 <span
+                                class="highlight rest-ticket">XXX</span> 張
                         </p>
                     </a>
                 </div>
