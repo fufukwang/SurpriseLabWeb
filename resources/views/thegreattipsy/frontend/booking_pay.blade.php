1217dc80572da377e5cf9d55680c1648b7ede40f
Fay Sie <sel12342001@gmail.com>
2020-12-07 23:48:48 +0800

    更新蛋奶素選項

--- resources/views/thegreattipsy/frontend/booking_pay.blade.php
+++ resources/views/thegreattipsy/frontend/booking_pay.blade.php
@@ -287,6 +287,10 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                             <option value="4">4</option>
                             <option value="5">5</option>
                             <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
                         </select>
                     </div>
 
