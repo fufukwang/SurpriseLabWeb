d3e9d19fe1c6c79444a1954698ebd430a43b3f0c
Fay Sie <sel12342001@gmail.com>
2021-01-06 18:02:28 +0800

    初始化主揪頁面架構

--- resources/views/thegreattipsy/frontend/master.blade.php
+++ resources/views/thegreattipsy/frontend/master.blade.php
@@ -1,1 +1,176 @@
-主揪
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Metadata -->
+    <meta charset="UTF-8">
+    <meta name="keywords" content="驚喜製造, 無光晚餐, 一人餐桌, 台北喝酒, 活動, 沉浸式體驗, 週末要幹麻, 好玩, 喝酒">
+    <meta name="description" content="歡迎來到《微醺大飯店》——那些你曾經希望能有所不同的事，在這都有可能發生。">
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content="微醺大飯店：1980s"/>
+    <meta property="og:site_name" content="微醺大飯店：1980s"/>
+    <meta property="og:description" content="歡迎來到《微醺大飯店》——那些你曾經希望能有所不同的事，在這都有可能發生。"/>
+    <meta property="og:url" content="https://www.surpriselab.com.tw/thegreattipsy"/>
+    <meta property="og:image" content="https://www.surpriselab.com.tw/thegreattipsy/img/opengraph.png"/>
+    <meta property="og:image:type" content="image/png"/>
+    <meta property="og:image:width" content="600"/>
+    <meta property="og:image:height" content="315"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=0">
+    <title>微醺大飯店：1980s</title>
+
+    <!-- Style -->
+    <link rel="icon" href="favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/plugins/bootstrap.css"/>
+
+    <!-- Ripple Animation Style -->
+    <link rel="stylesheet" type="text/css" href="css/plugins/loading.css"/>
+    <link rel="stylesheet" type="text/css" href="css/plugins/component.css"/>
+    <script>document.documentElement.className = 'js';</script>
+
+    <!-- Fontawesome -->
+    <link rel="stylesheet" type="text/css" href="css/font-awesome/fontawesome.min.css"/>
+
+    <!-- Select2 style -->
+    <link rel="stylesheet" href="css/plugins/select2.css"/>
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/style.css?v=0.1"/>
+    <link rel="stylesheet" href="css/form.css"/>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WCD7VGK');</script>
+    <!-- End Google Tag Manager -->
+    </head>
+    <body class="loading">
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WCD7VGK"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <main class="sections">
+        <!-- Header LOGO -->
+        <header class="logo">
+            <a href="index.html">
+                <img src="img/web_title_small.png" alt="The Great Tipsy">
+            </a>
+            <button class="hamburger">
+                <span class="current-section">Lobby</span>
+                <span class="current-menu">Menu</span>
+                <span class="menu">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </span>
+            </button>
+        </header>
+
+        <div class="container" id="bookingPage">
+            <!-- form -->
+            <form id="booking">
+                <!--progress title-->
+                <h3 class="progress-title">
+                    微醺大飯店：<span>1980s</span>
+                </h3>
+                <h5>賓客邀請函</h5>
+                <!-- progressbar -->
+                <div class="bar-wrapper hide-steps">
+                    <ul id="progressbar">
+                        <li class="active"></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+
+                <!-- fieldsets -->
+                <fieldset class="step step-1" title="微醺大飯店：1980s　賓客登記">
+                    <p class="sub-title text-lining">
+                        親愛的賓客您好，您為 <span class="color-yellow">驚喜製造</span> 所邀請之賓客<br/>
+                        並將於 <span class="color-yellow">2021/01/05 18:30</span> 前來
+                    </p>
+                    <div class="form-container">
+                        <p class="color-yellow">
+                            耳聞您即將前來微醺大飯店的開幕酒會
+                            飯店全體人員都相當期待
+                        </p>
+                        <p class="color-yellow">
+                            開幕酒會當日有知名歌星、企業家
+                            及諸多知名企業代表到場
+                        </p>
+                        <p class="color-yellow">
+                            為使酒會順利進行
+                            並避免閒雜人等之進入，身份控管尤其重要
+                        </p>
+                        <p>&nbsp;</p>
+                        <p>
+                            請於下方提供您的基本資訊，勾選同意後按下送出<br/>
+                            之後，您將收到一封非常重要的信，請務必收信。
+                        </p>
+                        <p class="prefix-step">
+                            <span class="prefix">Step 1</span>選擇欲劃位的人數<br/>
+                            <span class="prefix">Step 2</span>選擇日期、時段<br/>
+                            <span class="prefix">Step 3</span>填寫賓客資料<br/>
+                            <span class="prefix">Step 4</span>確認訂單，<span class="color-pink">輸入劃位序號</span><br/>
+                            <span class="prefix">Step 5</span>完成預約
+                        </p>
+                        <p>&nbsp;</p>
+                        <div class="sub-title-wrapper">
+                            <h4>
+                                <span>INFORMATION</span>
+                            </h4>
+                        </div>
+                        
+                    </div>
+
+                </fieldset>
+            </form>
+        </div>
+
+        <div class="submit-page success" id="bookingSuccess" style="display:none;">
+            <div class="container">
+                <h3 class="page-title">
+                    你已完成賓客預約！
+                </h3>
+                <p class="page-sub-title">
+                    請至你的信箱確認預約完成信件
+                </p>
+                <p class="page-info">
+                    接近體驗日期時<br/>
+                    將會寄送提醒信件與行前訊息給你<br/>
+                    <br/>
+                    屆時請注意電子信箱，它有時候會跑到垃圾郵件，可以翻找一下<br/><br/>
+                </p>
+                <div class="feature-btn" style="text-align: center;">
+                    <a class="btn-outline" href="index.html">完成</a>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- Bootstrap -->
+    <script src="js/plugins/jquery-3.3.1.min.js"></script>
+    <script src="js/plugins/jquery-ui.js"></script>
+    <script src="js/plugins/popper.min.js"></script>
+    <script src="js/plugins/bootstrap.min.js"></script>
+
+    <!-- Ripple Animation Js -->
+    <script src="js/plugins/pixi.min.js"></script>
+    <script src="js/plugins/TweenMax.min.js"></script>
+    <script src="js/plugins/custom-ripple.js"></script>
+    <script src="js/plugins/imagesloaded.pkgd.min.js"></script>
+
+    <!-- Select2 script-->
+    <script src="js/select2.js?v=1"></script>
+    <!-- blockUI script-->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.min.js"></script>
+    <!-- tappaysdk -->
+    <script src="https://js.tappaysdk.com/tpdirect/v5.4.0"></script>
+    <!-- Custom Js -->
+    <script src="js/form.js?v=0.15"></script>
+
+    </body>
+</html>
