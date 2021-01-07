2f468a37ddc0ed95c30f671d7c3173e3fbda75b5
Fay Sie <sel12342001@gmail.com>
2021-01-07 18:04:50 +0800

    修正主揪輸入框顏色顯示錯誤問題

--- resources/views/thegreattipsy/frontend/master.blade.php
+++ resources/views/thegreattipsy/frontend/master.blade.php
@@ -36,7 +36,7 @@
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/style.css?v=0.1"/>
     <link rel="stylesheet" href="css/form.css"/>
-    <link rel="stylesheet" href="css/master.css"/>
+    <link rel="stylesheet" href="css/master.css?=0.1"/>
 
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
