86112853bb9739c0aeeec2a06187d0c56a040acd
fufukwang <fu@thenewslens.com>
2021-01-14 17:28:48 +0800

    修改 command

--- app/Console/Commands/MasterMailSend.php
+++ app/Console/Commands/MasterMailSend.php
@@ -44,9 +44,19 @@ class MasterMailSend extends Command
     public function handle()
     {
         try {
+            // 定義時間
+            $day21 = 30240; // 24 * 60 * 21
+            $day14 = 20160; // 24 * 60 * 14
+            $day10 = 14400; // 24 * 60 * 10
+            $day05 = 7200; // 24 * 60 * 5
+            $day01 = 1440; // 24 * 60 * 1
             // 找出時間內的場次
-
-            // 場次抓出訂單
+            $pr21day = pro::select('id')->where('open',1)
+                ->whereRaw("floor(UNIX_TIMESTAMP(CONCAT(day,' ',rang_start))/1440)-floor(UNIX_TIMESTAMP()/1440)={$day21}")->get();
+            foreach($pr21day as $pro){
+                // 找出正常的訂單
+                //$order21 = order::
+            }
 
             // 送出信件
 
