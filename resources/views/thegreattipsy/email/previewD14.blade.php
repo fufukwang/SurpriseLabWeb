347d16b09f64b07643203499af1c4f8fb310898b
fufukwang <fu@thenewslens.com>
2021-04-08 23:28:32 +0800

    更新 14 天前信件

--- resources/views/thegreattipsy/email/previewD14.blade.php
+++ resources/views/thegreattipsy/email/previewD14.blade.php
@@ -1,15 +1,14 @@
 <!doctype html>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
     <head>
-		<meta charset="UTF-8">
+        <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>準備好以適合的樣貌前往過去了嗎？</title>
+        <title>準備好以適合的樣貌前往過去了嗎？</title>
+        
 <style type="text/css">p{margin:10px 0;padding:0}table{border-collapse:collapse}h1,h2,h3,h4,h5,h6{display:block;margin:0;padding:0}a img,img{border:0;height:auto;outline:0;text-decoration:none}#bodyCell,#bodyTable,body{height:100%;margin:0;padding:0;width:100%}.mcnPreviewText{display:none!important}#outlook a{padding:0}img{-ms-interpolation-mode:bicubic}table{mso-table-lspace:0;mso-table-rspace:0}.ReadMsgBody{width:100%}.ExternalClass{width:100%}a,blockquote,li,p,td{mso-line-height-rule:exactly}a[href^=sms],a[href^=tel]{color:inherit;cursor:default;text-decoration:none}a,blockquote,body,li,p,table,td{-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}.ExternalClass,.ExternalClass div,.ExternalClass font,.ExternalClass p,.ExternalClass span,.ExternalClass td{line-height:100%}a[x-apple-data-detectors]{color:inherit!important;text-decoration:none!important;font-size:inherit!important;font-family:inherit!important;font-weight:inherit!important;line-height:inherit!important}a.mcnButton{display:block}.mcnImage,.mcnRetinaImage{vertical-align:bottom}.mcnTextContent{word-break:break-word}.mcnTextContent img{height:auto!important}.mcnDividerBlock{table-layout:fixed!important}#bodyTable,#templateFooter,body{background-color:#281f1c}#bodyCell{border-top:0}#templateContainer{border:0}h1{color:#fff!important;display:block;font-family:Helvetica;font-size:40px;font-style:normal;font-weight:700;line-height:100%;letter-spacing:-1px;margin:0;text-align:center}h2{color:#000!important;display:block;font-family:Helvetica;font-size:36px;font-style:normal;font-weight:700;line-height:125%;letter-spacing:-.75px;margin:0;text-align:center}h3{color:#606060!important;display:block;font-family:Helvetica;font-size:18px;font-style:normal;font-weight:700;line-height:125%;letter-spacing:-.5px;margin:0;text-align:left}h4{color:grey!important;display:block;font-family:Helvetica;font-size:16px;font-style:normal;font-weight:700;line-height:125%;letter-spacing:normal;margin:0;text-align:left}#templatePreheader{background-color:#281f1c;border-top:0;border-bottom:0}.preheaderContainer .mcnTextContent,.preheaderContainer .mcnTextContent p{color:#000;font-family:Helvetica;font-size:11px;line-height:125%;text-align:left}.preheaderContainer .mcnTextContent a{color:#000;font-weight:400;text-decoration:underline}#templateHeader{background-color:#281f1c;border-top:0;border-bottom:0}.headerContainer .mcnTextContent,.headerContainer .mcnTextContent p{color:#000;font-family:Helvetica;font-size:16px;line-height:150%;text-align:center}.headerContainer .mcnTextContent a{color:#000;font-weight:400;text-decoration:underline}#templateBody{background-color:#281f1c;border-top:0;border-bottom:0}.bodyContainer .mcnTextContent,.bodyContainer .mcnTextContent p{color:#000;font-family:Helvetica;font-size:16px;line-height:150%;text-align:center}.bodyContainer .mcnTextContent a{color:#000;font-weight:400;text-decoration:underline}#templateColumns{background-color:#281f1c;border-top:0;border-bottom:0}.leftColumnContainer .mcnTextContent,.leftColumnContainer .mcnTextContent p{color:#000;font-family:Helvetica;font-size:14px;line-height:150%;text-align:center}.leftColumnContainer .mcnTextContent a{color:#000;font-weight:400;text-decoration:underline}.rightColumnContainer .mcnTextContent,.rightColumnContainer .mcnTextContent p{color:#000;font-family:Helvetica;font-size:14px;line-height:150%;text-align:center}.rightColumnContainer .mcnTextContent a{color:#000;font-weight:400;text-decoration:underline}#templateFooter{border-top:0;border-bottom:0}.footerContainer .mcnTextContent,.footerContainer .mcnTextContent p{color:#000;font-family:Helvetica;font-size:11px;line-height:125%;text-align:center}.footerContainer .mcnTextContent a{color:#000;font-weight:400;text-decoration:underline}@media only screen and (max-width:480px){a,blockquote,body,li,p,table,td{-webkit-text-size-adjust:none!important}}@media only screen and (max-width:480px){body{width:100%!important;min-width:100%!important}}@media only screen and (max-width:480px){#bodyCell{padding-top:10px!important}}@media only screen and (max-width:480px){.templateContainer{max-width:600px!important;width:100%!important}}@media only screen and (max-width:480px){.columnsContainer{display:block!important;max-width:600px!important;padding-bottom:18px!important;padding-left:0!important;width:100%!important}}@media only screen and (max-width:480px){.mcnRetinaImage{max-width:100%!important}}@media only screen and (max-width:480px){.mcnImage{height:auto!important;width:100%!important}}@media only screen and (max-width:480px){.mcnBoxedTextContentContainer,.mcnCaptionBottomContent,.mcnCaptionLeftImageContentContainer,.mcnCaptionLeftTextContentContainer,.mcnCaptionRightImageContentContainer,.mcnCaptionRightTextContentContainer,.mcnCaptionTopContent,.mcnCartContainer,.mcnImageCardLeftImageContentContainer,.mcnImageCardLeftTextContentContainer,.mcnImageCardRightImageContentContainer,.mcnImageCardRightTextContentContainer,.mcnImageGroupContentContainer,.mcnRecContentContainer,.mcnTextContentContainer{max-width:100%!important;width:100%!important}}@media only screen and (max-width:480px){.mcnBoxedTextContentContainer{min-width:100%!important}}@media only screen and (max-width:480px){.mcnImageGroupContent{padding:9px!important}}@media only screen and (max-width:480px){.mcnCaptionLeftContentOuter .mcnTextContent,.mcnCaptionRightContentOuter .mcnTextContent{padding-top:9px!important}}@media only screen and (max-width:480px){.mcnCaptionBlockInner .mcnCaptionTopContent:last-child .mcnTextContent,.mcnCaptionBottomContent:last-child .mcnCaptionBottomImageContent,.mcnImageCardTopImageContent{padding-top:18px!important}}@media only screen and (max-width:480px){.mcnImageCardBottomImageContent{padding-bottom:9px!important}}@media only screen and (max-width:480px){.mcnImageGroupBlockInner{padding-top:0!important;padding-bottom:0!important}}@media only screen and (max-width:480px){.mcnImageGroupBlockOuter{padding-top:9px!important;padding-bottom:9px!important}}@media only screen and (max-width:480px){.mcnBoxedTextContentColumn,.mcnTextContent{padding-right:18px!important;padding-left:18px!important}}@media only screen and (max-width:480px){.mcnImageCardLeftImageContent,.mcnImageCardRightImageContent{padding-right:18px!important;padding-bottom:0!important;padding-left:18px!important}}@media only screen and (max-width:480px){.mcpreview-image-uploader{display:none!important;width:100%!important}}@media only screen and (max-width:480px){h1{font-size:24px!important;line-height:125%!important}}@media only screen and (max-width:480px){h2{font-size:20px!important;line-height:125%!important}}@media only screen and (max-width:480px){h3{font-size:18px!important;line-height:125%!important}}@media only screen and (max-width:480px){h4{font-size:16px!important;line-height:125%!important}}@media only screen and (max-width:480px){.mcnBoxedTextContentContainer .mcnTextContent,.mcnBoxedTextContentContainer .mcnTextContent p{font-size:18px!important;line-height:125%!important}}@media only screen and (max-width:480px){#templatePreheader{display:block!important}}@media only screen and (max-width:480px){.preheaderContainer .mcnTextContent,.preheaderContainer .mcnTextContent p{font-size:14px!important;line-height:115%!important}}@media only screen and (max-width:480px){.headerContainer .mcnTextContent,.headerContainer .mcnTextContent p{font-size:18px!important;line-height:125%!important}}@media only screen and (max-width:480px){.bodyContainer .mcnTextContent,.bodyContainer .mcnTextContent p{font-size:18px!important;line-height:125%!important}}@media only screen and (max-width:480px){.leftColumnContainer .mcnTextContent,.leftColumnContainer .mcnTextContent p{font-size:18px!important;line-height:125%!important}}@media only screen and (max-width:480px){.rightColumnContainer .mcnTextContent,.rightColumnContainer .mcnTextContent p{font-size:18px!important;line-height:125%!important}}@media only screen and (max-width:480px){.footerContainer .mcnTextContent,.footerContainer .mcnTextContent p{font-size:14px!important;line-height:115%!important}}</style></head>
     <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
-        <!--*|IF:MC_PREVIEW_TEXT|*-->
-        <!--[if !gte mso 9]><!----><span class="mcnPreviewText" style="display:none; font-size:0px; line-height:0px; max-height:0px; max-width:0px; opacity:0; overflow:hidden; visibility:hidden; mso-hide:all;">準備好以適合的樣貌前往過去了嗎？</span><!--<![endif]-->
-        <!--*|END:IF|*-->
+        <span class="mcnPreviewText" style="display:none; font-size:0px; line-height:0px; max-height:0px; max-width:0px; opacity:0; overflow:hidden; visibility:hidden; mso-hide:all;">準備好以適合的樣貌前往過去了嗎？</span>
         <center>
             <table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">
                 <tr>
@@ -21,7 +20,7 @@
                                     <!-- BEGIN PREHEADER // -->
                                     <table border="0" cellpadding="0" cellspacing="0" width="100%" id="templatePreheader">
                                         <tr>
-                                        	<td align="center" valign="top">
+                                            <td align="center" valign="top">
                                                 <table border="0" cellpadding="0" cellspacing="0" width="600" class="templateContainer">
                                                     <tr>
                                                         <td valign="top" class="preheaderContainer" style="padding-top:10px; padding-bottom:10px;"></td>
@@ -80,14 +79,14 @@
     <tbody class="mcnTextBlockOuter">
         <tr>
             <td valign="top" class="mcnTextBlockInner" style="padding-top:9px;">
-              	<!--[if mso]>
-				<table align="left" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100%;">
-				<tr>
-				<![endif]-->
-			    
-				<!--[if mso]>
-				<td valign="top" width="600" style="width:600px;">
-				<![endif]-->
+                <!--[if mso]>
+                <table align="left" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100%;">
+                <tr>
+                <![endif]-->
+                
+                <!--[if mso]>
+                <td valign="top" width="600" style="width:600px;">
+                <![endif]-->
                 <table align="left" border="0" cellpadding="0" cellspacing="0" style="max-width:100%; min-width:100%;" width="100%" class="mcnTextContentContainer">
                     <tbody><tr>
                         
@@ -102,7 +101,7 @@
 不妨開啟 <em><strong><u><a data-target-href="https://surpriselab.typeform.com/to/f3AXYSBI" href="https://surpriselab.typeform.com/to/f3AXYSBI" rel="noreferrer nofollow noopener" target="_blank"><span style="color:#e5ad5c">開幕酒會邀請函</span></a></u></strong></em>，內有實用的服裝建議。<br>
 <br>
 除此之外，在微醺大飯店開幕酒會上，<br>
-香港女星梁如嫣將獻唱她的經典舞曲 ——<em><strong><u><a href="https://drive.google.com/file/d/15G646hikiCjNi_83hQd4TCfl2d4r7nQm/view?usp=sharing"><span style="color:#e5ad5c">〈</span></a><a href="https://drive.google.com/file/d/15G646hikiCjNi_83hQd4TCfl2d4r7nQm/view?usp=sharing"><span style="color:#e5ad5c">Dance</span></a><a href="https://drive.google.com/file/d/15G646hikiCjNi_83hQd4TCfl2d4r7nQm/view?usp=sharing"><span style="color:#e5ad5c"> With Me</span></a><a href="https://drive.google.com/file/d/15G646hikiCjNi_83hQd4TCfl2d4r7nQm/view?usp=sharing"><span style="color:#e5ad5c">〉</span></a></u></strong></em>。<br>
+香港女星梁如嫣將獻唱她的經典舞曲 ——<strong><span style="color:#e5ad5c">〈Dance With Me〉</span></strong>。<br>
 <br>
 屆時請大聲與她一起同唱，<br>
 也別吝嗇您的熱烈掌聲。<br>
@@ -114,30 +113,14 @@
                         </td>
                     </tr>
                 </tbody></table>
-				<!--[if mso]>
-				</td>
-				<![endif]-->
+                <!--[if mso]>
+                </td>
+                <![endif]-->
                 
-				<!--[if mso]>
-				</tr>
-				</table>
-				<![endif]-->
-            </td>
-        </tr>
-    </tbody>
-</table><table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnButtonBlock" style="min-width:100%;">
-    <tbody class="mcnButtonBlockOuter">
-        <tr>
-            <td style="padding-top:0; padding-right:18px; padding-bottom:18px; padding-left:18px;" valign="top" align="center" class="mcnButtonBlockInner">
-                <table border="0" cellpadding="0" cellspacing="0" class="mcnButtonContentContainer" style="border-collapse: separate !important;border-radius: 12px;background-color: #E5AD5C;">
-                    <tbody>
-                        <tr>
-                            <td align="center" valign="middle" class="mcnButtonContent" style="font-family: &quot;Lucida Sans Unicode&quot;, &quot;Lucida Grande&quot;, sans-serif; font-size: 14px; padding: 18px;">
-                                <a class="mcnButton " title="搶先聽聽〈Dance With Me〉" href="https://drive.google.com/file/d/15G646hikiCjNi_83hQd4TCfl2d4r7nQm/view?usp=sharing" target="_blank" style="font-weight: bold;letter-spacing: 1px;line-height: 100%;text-align: center;text-decoration: none;color: #000000;">搶先聽聽〈Dance With Me〉</a>
-                            </td>
-                        </tr>
-                    </tbody>
+                <!--[if mso]>
+                </tr>
                 </table>
+                <![endif]-->
             </td>
         </tr>
     </tbody>
@@ -145,14 +128,14 @@
     <tbody class="mcnTextBlockOuter">
         <tr>
             <td valign="top" class="mcnTextBlockInner" style="padding-top:9px;">
-              	<!--[if mso]>
-				<table align="left" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100%;">
-				<tr>
-				<![endif]-->
-			    
-				<!--[if mso]>
-				<td valign="top" width="600" style="width:600px;">
-				<![endif]-->
+                <!--[if mso]>
+                <table align="left" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100%;">
+                <tr>
+                <![endif]-->
+                
+                <!--[if mso]>
+                <td valign="top" width="600" style="width:600px;">
+                <![endif]-->
                 <table align="left" border="0" cellpadding="0" cellspacing="0" style="max-width:100%; min-width:100%;" width="100%" class="mcnTextContentContainer">
                     <tbody><tr>
                         
@@ -168,14 +151,14 @@
                         </td>
                     </tr>
                 </tbody></table>
-				<!--[if mso]>
-				</td>
-				<![endif]-->
+                <!--[if mso]>
+                </td>
+                <![endif]-->
                 
-				<!--[if mso]>
-				</tr>
-				</table>
-				<![endif]-->
+                <!--[if mso]>
+                </tr>
+                </table>
+                <![endif]-->
             </td>
         </tr>
     </tbody>
@@ -411,14 +394,14 @@
     <tbody class="mcnTextBlockOuter">
         <tr>
             <td valign="top" class="mcnTextBlockInner" style="padding-top:9px;">
-              	<!--[if mso]>
-				<table align="left" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100%;">
-				<tr>
-				<![endif]-->
-			    
-				<!--[if mso]>
-				<td valign="top" width="600" style="width:600px;">
-				<![endif]-->
+                <!--[if mso]>
+                <table align="left" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100%;">
+                <tr>
+                <![endif]-->
+                
+                <!--[if mso]>
+                <td valign="top" width="600" style="width:600px;">
+                <![endif]-->
                 <table align="left" border="0" cellpadding="0" cellspacing="0" style="max-width:100%; min-width:100%;" width="100%" class="mcnTextContentContainer">
                     <tbody><tr>
                         <td valign="top" class="mcnTextContent" style="padding-top:0; padding-right:18px; padding-bottom:9px; padding-left:18px;">
@@ -430,14 +413,14 @@
                         </td>
                     </tr>
                 </tbody></table>
-				<!--[if mso]>
-				</td>
-				<![endif]-->
+                <!--[if mso]>
+                </td>
+                <![endif]-->
                 
-				<!--[if mso]>
-				</tr>
-				</table>
-				<![endif]-->
+                <!--[if mso]>
+                </tr>
+                </table>
+                <![endif]-->
             </td>
         </tr>
     </tbody>
