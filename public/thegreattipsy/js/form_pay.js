c13f2d6ea8e47b95c0d6b3a5c6311d9e0e7cd15e
fufukwang <fu@thenewslens.com>
2021-04-16 20:32:46 +0800

    拿掉服務費

--- public/thegreattipsy/js/form_pay.js
+++ public/thegreattipsy/js/form_pay.js
@@ -765,7 +765,7 @@ function SendOrderData(Pay,prime){
         'prime' : prime,
         'Pay'   : Pay,
         'coupon': usedCoupons,
-        'is_overseas':1,
+        'is_overseas':2,
         'vegetarian': $('#vegetarian_food').val(),
     };
     $.post('/thegreattipsy/ReOrderData',obj,function(data){
