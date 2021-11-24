67f0205416fa55f743a6e64ff71d77ceb654b173
Fu <fu@thenewslens.com>
2021-11-24 12:53:12 +0800

    調整簡訊內容

--- app/Http/Controllers/tgt2/FrontController.php
+++ app/Http/Controllers/tgt2/FrontController.php
@@ -283,7 +283,7 @@ class FrontController extends Controller
                     });
                     $order->is_send = 1;
                     $order->save();
-                    SLS::sent_single_sms($order->tel,"《微醺大飯店：1980s》訂位確認信已寄出，內含重要任務，請務必、務必查看。若未收到，請至垃圾信匣或促銷內容分類尋找唷！\n\n非常期待與您見面。\n\n順安, 微醺大飯店：1980s");
+                    SLS::sent_single_sms($order->tel,"《微醺大飯店：1980s》訂位確認信已寄出，內含重要任務，請務必、務必查看。若未收到，請至促銷內容分類尋找，也歡迎來信客服信箱詢問！\n\n非常期待與您見面。\n\n順安, 微醺大飯店：1980s");
                     // 信件補送
                     $now = time();
                     $lim = strtotime($act->day.' '.$act->rang_start);
