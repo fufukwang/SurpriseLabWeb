d418cd0391bec3568ebfc87d096155e6fb26339d
Fu <fu@thenewslens.com>
2022-02-09 17:06:44 +0800

    update date

--- public/thegreattipsy/js/form_pay.js
+++ public/thegreattipsy/js/form_pay.js
@@ -84,7 +84,7 @@ $(".action-button").on('click', function(){
                 booking_date.datepicker("destroy");
                 booking_date.datepicker({
                     minDate: minD,
-                    maxDate: new Date(2022, 2, 28),
+                    maxDate: new Date(2022, 1, 28),
                     dateFormat: 'yy-mm-dd', 
                     beforeShowDay: enableAllTheseDays
                 });
