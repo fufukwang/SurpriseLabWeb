0a3cabc01716dfd2c1450765526e7ee50452b682
Fu <fu@thenewslens.com>
2022-03-28 15:40:58 +0800

    更新區間 & 調整人數

--- public/thegreattipsy/js/form_pay.js
+++ public/thegreattipsy/js/form_pay.js
@@ -86,7 +86,7 @@ $(".action-button").on('click', function(){
                 booking_date.datepicker("destroy");
                 booking_date.datepicker({
                     minDate: "+3d",// minD,
-                    maxDate: "+21d",// new Date(2022, 1, 28),
+                    maxDate: new Date(2022, 4, 1),//"+21d",// new Date(2022, 1, 28),
                     dateFormat: 'yy-mm-dd', 
                     beforeShowDay: enableAllTheseDays
                 });
