48a966e13a44da3998150c6ba1eedba9b8956a25
Fay Sie <sel12342001@gmail.com>
2022-05-17 17:09:08 +0800

    更新無光三壁紙樣式

--- resources/views/dininginthedark3/frontend/home.blade.php
+++ resources/views/dininginthedark3/frontend/home.blade.php
@@ -414,7 +414,7 @@
             
             <h2>Contact</h2>
 
-            <div class="section sec-contact" data-aos="fade">
+            <div class="fullpage section sec-contact" data-aos="fade">
                 <div class="container">
 
                     <div class="section-header">
