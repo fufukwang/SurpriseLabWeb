5e160f7a4c75efb4cf10d783bc446cddd8e1c215
Peter <ericlu0814@cosmo-br.com>
2023-02-24 18:09:18 +0800

    [無光]觀眾評價 切換功能圖示更改

--- public/dininginthedark3/js/index-slideshow.js
+++ public/dininginthedark3/js/index-slideshow.js
@@ -29,4 +29,43 @@ $(function() {
             }
         });
     });
+
+    let $owlReview = $('#owl-review-01');
+    // 觸發 Owl Carousel
+    $owlReview.each(function(){
+        $(this).owlCarousel({
+            center: true,
+            loop: false,
+            nav: true,
+            navText: ['<img src="img/home/landing/btn-left.svg" alt="btn-left"/>', '<img src="img/home/landing/btn-right.svg" alt="btn-right"/>'],
+            lazyLoad: true,
+            responsive: {
+                0: {
+                    items: 1.24,
+                    dots: false,
+                    margin: 20
+                },
+                767: {
+                    items: 3.1,
+                    dots: false,
+                    margin: 20
+                },
+                992: {
+                    items: 3.5,
+                    dots: false,
+                    margin: 20
+                },
+                1200: {
+                    items: 3.5,
+                    dots: false,
+                    margin: 60
+                },
+                1441: {
+                    items: 5,
+                    dots: false,
+                    margin: 60
+                }
+            }
+        });
+    });
 });
