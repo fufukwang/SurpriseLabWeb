254290c665a4b6eace73b2e37e7161b6f5a51206
Tiffany Keng <tiffk935@gmail.com>
2023-08-01 09:42:34 +0800

    取消從後端抓maxDate

--- resources/views/dininginthedark3/frontend/booking_pay.blade.php
+++ resources/views/dininginthedark3/frontend/booking_pay.blade.php
@@ -673,7 +673,7 @@
     @endif
     --}}
     <!-- Custom Js -->
-    <script src="js/form_pay.js?230801"></script>
+    <script src="js/form_pay.js?2308012"></script>
 @if($day!='')
     @if($code == 'pass')
     <script type="text/javascript">$(function(){ goDay('{{ $day }}'); });</script>
