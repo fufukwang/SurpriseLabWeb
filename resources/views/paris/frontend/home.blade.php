af1625032cf9e53f03ce7b31dad6538f5a10613f
Tiffany Keng <tiffk935@gmail.com>
2023-10-03 12:15:55 +0800

    首頁

--- resources/views/paris/frontend/home.blade.php
+++ resources/views/paris/frontend/home.blade.php
@@ -9,19 +9,425 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;500;700&family=Noto+Serif+TC:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.typekit.net/vpt1inn.css">
-    <link rel="stylesheet" href="/paris/css/booking.css?2309270">
+    <link rel="stylesheet" href="paris/css/plugins/owl.carousel.min.css">
+    <link rel="stylesheet" href="paris/css/home.css">
 </head>
 <body>
-    
-    @include('paris.frontend._nav')
+    <div id="banner">
+        <img class="logo" src="/paris/img/logo.png" />
+        <div class="bottom">
+            <div class="enter">ENTER</div>
+            <img class="banner-logos pc" src="/paris/img/banner-logos.png" />
+            <img class="banner-logos m" src="/paris/img/banner-logos-m.png" />
+        </div>
+    </div>
 
-    <main id="booking-fail">
-        <center>巴黎舞會首頁</center>
+    <header id="main-header">
+        <div class="flex justify-between items-center">
+            <div class="brand-logo">
+                <a href="/lebaldeparis">
+                    <img class="pc" src="/paris/img/brand-logo.png" alt="巴黎舞會 LE BAL PARIS" />
+                    <img class="m" src="/paris/img/brand-logo-m.png" alt="巴黎舞會 LE BAL PARIS" />
+                </a>
+            </div>
+            <div class="mob-toggle m"></div>
+        </div>
+        <nav>
+            <a class="scrollto active" href="/lebaldeparis#sec-about" data-sec="about">關於舞會</a>
+            <a class="scrollto" href="/lebaldeparis#sec-booking" data-sec="booking">購票資訊</a>
+            <a class="scrollto" href="/lebaldeparis#sec-play" data-sec="play">如何享受</a>
+            <a class="scrollto" href="/lebaldeparis#sec-review" data-sec="review">口碑評價</a>
+            <a class="scrollto" href="/lebaldeparis#sec-group" data-sec="group">創作團隊</a>
+            <a class="scrollto" href="/lebaldeparis#sec-information" data-sec="information">舞會須知</a>
+            <a class="scrollto" href="/lebaldeparis#sec-partner" data-sec="partner">合作夥伴</a>
+            <a href="/lebaldeparis/booking">預約舞會</a>
+        </nav>
+    </header>
+
+    <main id="home">
+        <section id="sec-about">
+            <div class="title">
+                <img src="/paris/img/deco.svg" />
+                <h1>關於舞會</h1>
+                <h3>Le Bal</h3>
+            </div>
+    
+            <div class="info">
+                親愛的賓客，歡迎來到巴黎舞會<br>
+                <br>
+                隱身於面具下、穿起高級訂製服<br>
+                那些煩惱、包袱、擔憂，都能在此拋下<br>
+                <br>
+                從湖泊、花園、華爾滋到狂歡秀<br>
+                那些奢華、神秘、奇幻，都能在這尋得<br>
+                <br>
+                來吧，邀上你想共舞的那個人<br>
+                一起跳脫現實，跨越虛實界線<br>
+                <br>
+                來巴黎舞會<br>
+                跳場最自由的舞，做一場最真實的夢<br>
+                <br>
+                Now, should we dance?
+            </div>
+    
+            <div class="about">
+                <div class="item">
+                    <div class="icon">
+                        <img src="/paris/img/about1.png" />
+                    </div>
+                    <h4>威尼斯影展<br>最佳 VR 體驗</h4>
+                    <div class="desc">自由移動、真實互動的感官體驗</div>
+                </div>
+                <div class="item">
+                    <div class="icon">
+                        <img src="/paris/img/about2.png" />
+                    </div>
+                    <h4>香奈兒<br>高級訂製服</h4>
+                    <div class="desc">自由選擇虛擬世界的舞會服裝</div>
+                </div>
+                <div class="item">
+                    <div class="icon">
+                        <img src="/paris/img/about3.png" />
+                    </div>
+                    <h4>80 分鐘<br>沉浸頂級晚宴</h4>
+                    <div class="desc">穿越 1920 年代的花園、舞會和狂歡</div>
+                </div>
+            </div>
+
+            <div class="yt">
+                <div class="vid-container">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/o1VLn3f2jyE?si=C9KgGWZZqoEDphXY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+            </div>
+        </section>
+
+        <section id="sec-booking">
+            <div class="title">
+                <img src="/paris/img/deco.svg" />
+                <h1>購票資訊</h1>
+                <h3>Ticket</h3>
+            </div>
+            
+            <div class="price pc">
+                <div class="item">
+                    <div class="icon">
+                        <img src="/paris/img/booking_ticket1.png" />
+                    </div>
+                    <h4>單人獨舞票</h4>
+                    <h2>$2,000</h2>
+                    <div class="desc">
+                        （含一杯調飲）<br>
+                        <br>
+                        一個人自由地探索<br>
+                        帶著好奇心闖入花漾年代
+                    </div>
+                    <a class="p-btn primary" href="/lebaldeparis/booking">預約舞會</a>
+                </div>
+                <div class="item">
+                    <div class="icon">
+                        <img src="/paris/img/booking_ticket2.png" />
+                    </div>
+                    <h4>雙人共舞票</h4>
+                    <h2>$3,800<span>($1,900/人)</span></h2>
+                    <div class="desc">
+                        （含兩杯調飲）<br>
+                        <br>
+                        邀請最重要的人<br>
+                        感受奢華絢爛的巴黎午夜
+                    </div>
+                    <a class="p-btn primary" href="/lebaldeparis/booking">預約舞會</a>
+                </div>
+                <div class="item">
+                    <div class="icon">
+                        <img src="/paris/img/booking_ticket3.png" />
+                    </div>
+                    <h4>四人群舞票</h4>
+                    <h2>$7,400<span>($1,850/人)</span></h2>
+                    <div class="desc">
+                        （含四杯調飲）<br>
+                        <br>
+                        與熟悉的人狂歡<br>
+                        摸索與彼此的第一支舞
+                    </div>
+                    <a class="p-btn primary" href="/lebaldeparis/booking">預約舞會</a>
+                </div>
+            </div>
+
+            <div class="owl-carousel price m">
+                <div class="item">
+                    <div class="icon">
+                        <img src="/paris/img/booking_ticket1.png" />
+                    </div>
+                    <h4>單人獨舞票</h4>
+                    <h2>$2,000</h2>
+                    <div class="desc">
+                        （含一杯調飲）<br>
+                        <br>
+                        一個人自由地探索<br>
+                        帶著好奇心闖入花漾年代
+                    </div>
+                    <a class="p-btn primary" href="/lebaldeparis/booking">預約舞會</a>
+                </div>
+                <div class="item">
+                    <div class="icon">
+                        <img src="/paris/img/booking_ticket2.png" />
+                    </div>
+                    <h4>雙人共舞票</h4>
+                    <h2>$3,800<span>($1,900/人)</span></h2>
+                    <div class="desc">
+                        （含兩杯調飲）<br>
+                        <br>
+                        邀請最重要的人<br>
+                        感受奢華絢爛的巴黎午夜
+                    </div>
+                    <a class="p-btn primary" href="/lebaldeparis/booking">預約舞會</a>
+                </div>
+                <div class="item">
+                    <div class="icon">
+                        <img src="/paris/img/booking_ticket3.png" />
+                    </div>
+                    <h4>四人群舞票</h4>
+                    <h2>$7,400<span>($1,850/人)</span></h2>
+                    <div class="desc">
+                        （含四杯調飲）<br>
+                        <br>
+                        與熟悉的人狂歡<br>
+                        摸索與彼此的第一支舞
+                    </div>
+                    <a class="p-btn primary" href="/lebaldeparis/booking">預約舞會</a>
+                </div>
+            </div>
+        </section>
+        
+        <section id="sec-play">
+            <div class="title">
+                <img src="/paris/img/deco.svg" />
+                <h1>如何享受</h1>
+                <h3>PLAY</h3>
+            </div>
+    
+            <div class="play">
+                <div class="item">
+                    <div class="icon">
+                        <img src="/paris/img/play1.png" />
+                    </div>
+                    <h4>玩法一：放下包袱</h4>
+                    <div class="desc">
+                        在面具之下，沒人知道你是誰<br>
+                        丟下所有顧慮，自由起舞吧！
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="icon">
+                        <img src="/paris/img/play2.png" />
+                    </div>
+                    <h4>玩法二：大膽探索</h4>
+                    <div class="desc">
+                        你將戴上頭顯、綁上追蹤器<br>
+                        自由移動在虛擬又巨大的場景中
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="icon">
+                        <img src="/paris/img/play3.png" />
+                    </div>
+                    <h4>玩法三：打開感官</h4>
+                    <div class="desc">
+                        一個人自由地探索<br>
+                        帶著好奇心闖入花漾年代
+                    </div>
+                </div>
+            </div>
+    
+            <div class="info">
+                <br>
+                <br>
+                <h4>這將是一場你從未歷經過的體驗</h4>
+                <br>
+                <div>你已受邀請參加巴黎最盛大的舞會！音樂與舞蹈與虛擬現實融合，為你帶來藝術和壯觀的歡樂體驗。無需熟悉舞蹈或虛擬現實，即可投入這場讓你漫遊於不同世界的舞會。</div>
+            </div>
+
+            <div class="slider">
+                <div class="owl-carousel">
+                    <div>
+                        <img src="/paris/img/vr1.jpg" />
+                    </div>
+                    <div>
+                        <img src="/paris/img/vr2.jpg" />
+                    </div>
+                    <div>
+                        <img src="/paris/img/vr3.jpg" />
+                    </div>
+                    <div>
+                        <img src="/paris/img/vr4.jpg" />
+                    </div>
+                    <div>
+                        <img src="/paris/img/vr5.jpg" />
+                    </div>
+                    <div>
+                        <img src="/paris/img/vr6.jpg" />
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section id="sec-more">
+            <div class="title">
+                <img src="/paris/img/deco.svg" />
+                <h1>舞會嘉賓</h1>
+                <h3>MORE</h3>
+            </div>
+    
+            <div class="more">
+                <div class="more-inner">
+                    <img src="/paris/img/more1.jpg" />
+                    <img src="/paris/img/more2.jpg" />
+                </div>
+            </div>
+        </section>
+
+        <section id="sec-review">
+            <div class="title">
+                <img src="/paris/img/deco.svg" />
+                <h1>口碑評價</h1>
+                <h3>Review</h3>
+            </div>
+
+            <div class="slider">
+                <div class="owl-carousel">
+                    <div>
+                        <img src="/paris/img/review1.png" />
+                    </div>
+                    <div>
+                        <img src="/paris/img/review2.png" />
+                    </div>
+                    <div>
+                        <img src="/paris/img/review3.png" />
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="sec-group">
+            <div class="title">
+                <img src="/paris/img/deco.svg" />
+                <h1>BLANCA LI</h1>
+                <h3>創作團隊</h3>
+            </div>
+    
+            <div class="blancali">
+                <img class="photo" src="/paris/img/blancali.jpg" />
+                <div class="desc">
+                    Blanca Li （布蘭卡˙李）<br>
+                    <br>
+                    西班牙跨界舞蹈鬼才，身兼編舞家、舞者、導演，新科技在她的各種創作中始終佔有特殊的位置。<br>
+                    <br>
+                    曾受邀為巴黎歌劇院及紐約大都會歌劇院委託創作，也曾為佩卓˙阿莫多瓦 (Pedro Almodovar)、米歇爾˙龔特利 (Michael Gondry)、傻瓜龐克 (Daft Punk)、碧昂絲 (Beyoncé) 、酷玩樂團 (Coldplay) 等音樂人合作編舞。<br>
+                    <br>
+                    《巴黎舞會》由 Blanca Li 歷時四年、動用超過百名人力，並一舉奪下 2021 年威尼斯影展最佳 VR 體驗獎，除了選擇台灣作為亞洲首演地，《巴黎舞會》也預計於 2023 至坎城、新加坡、羅馬、紐約、上海演出。
+                </div>
+            </div>
+        </section>
+        
+        <section id="sec-information">
+            <div class="title">
+                <img src="/paris/img/deco.svg" />
+                <h1>舞會須知</h1>
+                <h3>Information</h3>
+            </div>
+    
+            <div class="information">
+                <div class="information-inner">
+                    <div class="col">
+                        <div class="grid">
+                            <h4>地點</h4>
+                            <p>兒童新樂園步行 3 分鐘處<br>（詳細地點請容許我們先保密，我們將慢慢向你揭露）</p>
+                        </div>
+                        <div class="grid">
+                            <h4>場次</h4>
+                            <p>
+                                每週一 - 二｜公休<br>
+                                每週三 - 五<br>
+                                晚場：18:10｜19:30｜ 20:45
+                            </p>
+                            <p>
+                                每週六 - 日<br>
+                                午場：13:20｜14:45｜16:00<br>
+                                晚場：18:10｜19:30｜ 20:45
+                            </p>
+                            <p>體驗時長：80 分鐘</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="grid">
+                            <h4>注意事項</h4>
+                            <ul>
+                                <li>
+                                    <p>
+                                        現場包含 VR 體驗，為顧及體驗安全：
+                                        <ul>
+                                            <li>孕婦、心臟病和高血壓患者不建議體驗</li>
+                                            <li>體內有電子儀器者（例：心律調整器、助聽器、除顫器）及患有癲癇容易暈倒、視力受損者下訂前需諮詢專業醫師以維護體驗安全</li>
+                                            <li>未滿 12 歲無法參與、13-16 歲需家長陪同</li>
+                                        </ul>
+                                    </p>
+                                </li>
+                                <li><p>包場需求、其他特殊服務，請來信客服詢問<br><a class="hover-underline" href="mailto:lebaldeparis@surpriselab.com.tw">lebaldeparis@surpriselab.com.tw</a></p></li>
+                                <li><p>相關購票、退票、改期規則，請見 <a class="hover-underline" href="#">[舞會規則]</a></p></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="actions">
+                    <a class="p-btn primary" href="/lebaldeparis/rules">更多須知</a>
+                </div>
+            </div>
+        </section>
+        
+        <section id="sec-partner">
+            <div class="title">
+                <img src="/paris/img/deco.svg" />
+                <h1>合作夥伴</h1>
+                <h3>Partner</h3>
+            </div>
+    
+            <div class="partner">
+                <div class="logo">
+                    <img src="/paris/img/partner-blanca.png" />
+                </div>
+                <div class="logo">
+                    <img src="/paris/img/partner-backLight.png" />
+                </div>
+                <div class="logo">
+                    <img src="/paris/img/partner-chanel.png" />
+                </div>
+                <div class="logo">
+                    <img src="/paris/img/partner-ministry.png" />
+                </div>
+                <div class="logo">
+                    <img src="/paris/img/partner-taicca.png" />
+                </div>
+                <div class="logo">
+                    <img src="/paris/img/partner-vive.png" />
+                </div>
+                <div class="logo">
+                    <img src="/paris/img/partner-surpriseLab.png" />
+                </div>
+            </div>
+        </section>
     </main>
 
     @include('paris.frontend._footer')
 
+    <a id="fixed-booking" href="/lebaldeparis/booking" style="display: none;">
+        <img src="paris/img/fixed-booking.png" />
+    </a>
+
     <script src="/paris/js/plugins/jquery-3.3.1.min.js"></script>
+    <script src="/paris/js/plugins/owl.carousel.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
     <script src="/paris/js/main.js?231003"></script>
+    <script src="/paris/js/home.js"></script>
 </body>
 </html>
