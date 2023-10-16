975e28b7209252d26fdae96df9c3a0e255315ad6
Tiffany Keng <tiffk935@gmail.com>
2023-10-16 11:30:49 +0800

    隱藏早鳥

--- resources/views/paris/frontend/home.blade.php
+++ resources/views/paris/frontend/home.blade.php
@@ -150,13 +150,13 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                 </div>
                 <div class="item">
                     <div class="icon">
-                        <img src="/paris/img/booking_ticket3.png" />
+                        <img src="/paris/img/booking_ticket3_old.png" />
                     </div>
                     <h4>四人群舞票</h4>
                     <h2>$7,400<span>($1,850/人)</span></h2>
                     <div class="desc">
                         （含四杯調飲）<br>
-                        <br>
+                        <br> 
                         與熟悉的人狂歡<br>
                         摸索與彼此的第一支舞
                     </div>
